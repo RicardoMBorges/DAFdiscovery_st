072538decc75183fddcf57053ee080bbb9525084
Ricardo M Borges <ricardo_mborges@yahoo.com.br>
2025-10-22 14:56:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -891,7 +891,7 @@ st.sidebar.markdown("""
 </center>
 """, unsafe_allow_html=True)
 
-VIDEO_URL = "https://youtu.be/8XEeGcwKeuc"
+VIDEO_URL = "https://youtu.be/1mqymNaGHHs"
 
 st.sidebar.markdown("""
 <hr>
@@ -1176,4 +1176,5 @@ Use this feature when you want to force the correlation analysis of a specific s
 # Load the pipeline DAFdiscovery
 pipeline = Image.open("static/pipeline.png")
 # Display the pipeline in the sidebar or header
+
 st.image(pipeline, width=900)
